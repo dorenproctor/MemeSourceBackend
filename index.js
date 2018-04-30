4313443131f7093812a20b9d2d8a3f892b0b8f07
Doren Proctor <dorenproctor@gmail.com>
2018-04-30 15:15:16 -0700

    Renamed and reordered some of the commands Created a list of them at top of file

--- index.js
+++ index.js
@@ -23,6 +23,173 @@ db.once('open', function() {
     console.log("Connected to Database");
 });
 
+/* available commands:
+    /imageInfo/:id
+    /getImage/:id
+    /commentInfo/:id
+    /postComment //takes a json object with post call
+    /deleteComment/:id
+    /createUser //takes a json object with post call
+    /listUsers
+    /signIn/:username/:password
+    /upvoteImage/:username/:imageId
+    /downvoteImage/:username/:imageId
+*/
+
+
+app.get('/imageInfo/:id', function(req, res) {
+    ImageModel.findOne({"imageId": req.params.id}, function(err, dbEntry) {
+        if (err) {
+            console.log(err);
+            res.send(err);
+        } else {
+            console.log(dbEntry);
+            res.send(dbEntry);
+        }
+    });
+});
+
+
+app.get('/getImage/:id', function(req, res) {
+    var params = {
+        Bucket: 'memesourceimages',
+        Key : req.params.id+'.jpg'
+    };
+    s3.getObject(params, function (err, data) {
+        if (err) {
+            console.log("Error: ", err);
+            res.send(err);
+        }
+        if (data) {
+            // data.Metadata = dbEntry;
+            const response = {
+                    statusCode: 200,
+                data: data
+            }
+            console.log("Data: ", data);
+            res.send(response);
+        }
+    });
+});
+
+
+app.get('/commentInfo/:id', function(req, res) {
+    CommentModel.find({"imageId": req.params.id}, function(err, comments) {
+        if (err) {
+            console.log(err);
+            res.send(err);
+        } else if (!comments) {
+            console.log("Image id not found");
+            res.send("Image id not found");
+        } else {
+            const response = {
+                statusCode: 200,
+                comments: comments
+            }
+            console.log(comments);
+            res.send(comments);
+        }
+    });
+});
+
+
+app.post('/postComment', function(req, res){
+    if ((req.body.imageId) && req.body.content && req.body.user) {
+        var commentData = {
+            imageId: req.body.imageId,
+            content: req.body.content,
+            user: req.body.user
+        };
+        CommentModel.create(commentData, function (err, user) {
+            if (err) {
+                console.log(err);
+                res.send(err);
+            } else {
+                console.log("Created comment", req.body.content);
+                res.send(({statusCode: 200}));
+            }
+        });
+    } else {
+        res.send("The proper parameters were not passed");
+    }
+});
+
+
+app.delete('/deleteComment/:id', function(req, res) {
+    CommentModel.findByIdAndRemove(req.params.id, function(err, removed) {
+        if (err) {
+            console.log(err);
+            res.send(err);
+        } else if (!removed) {
+            console.log("Comment not found to delete");
+            res.send("Comment not found to delete");
+        } else {
+            console.log("Comment removed: ", removed);
+            res.send({ statusCode: 200 });
+        }
+    });
+});
+
+
+app.post('/createUser', function(req, res){
+if (req.body.email && req.body.username &&
+    req.body.password && req.body.passwordConf) {
+        var userData = {
+            email: req.body.email,
+            username: req.body.username,
+            password: req.body.password,
+            passwordConf: req.body.passwordConf,
+        }
+        User.create(userData, function (err, user) {
+            if (err) {
+                console.log(err);
+                res.send(err);
+            } else {
+                console.log("Created user " + req.body.username);
+                res.send({ statusCode: 200 });
+            }
+        });
+    }
+});
+
+
+app.get('/listUsers', function(req, res) {
+    User.find(function(err, users) {
+        if (err) {
+            console.log(err);
+            res.send(err);
+        } else {
+            console.log(users);
+            res.send(users);
+        }
+    });
+});
+
+
+app.get('/signIn/:username/:password', function(req, res) {
+    User.findOne({"username": req.params.username}, function(err, user) {
+        if (err) {
+            console.log(err);
+            res.send(err);
+        } else if (!user) {
+            console.log("Didn't find user");
+            res.send("Didn't find user");
+        } else {
+            if (req.params.password === user.password) {
+                console.log(user.username, " signed in");
+                res.send({statusCode: 200});
+            } else {
+                console.log(user, user.username, user.password, req.params.username, req.params.password);
+                console.log("Password doesn't match");
+                res.send("Password doesn't match");
+            }
+        }
+    });
+});
+
+
+
+
 
 app.put('/upvoteImage/:username/:imageId', function(req, res) {
     var username = req.params.username;
@@ -130,151 +297,7 @@ app.put('/downvoteImage/:username/:imageId', function(req, res) {
 });
 
 
-app.post('/postComment', function(req, res){
-    if ((req.body.imageId) && req.body.content && req.body.user) {
-        var commentData = {
-            imageId: req.body.imageId,
-            content: req.body.content,
-            user: req.body.user
-        };
-        CommentModel.create(commentData, function (err, user) {
-            if (err) {
-                console.log(err);
-                res.send(err);
-            } else {
-                console.log("Created comment", req.body.content);
-                res.send(({statusCode: 200}));
-            }
-        });
-    } else {
-        res.send("The proper parameters were not passed");
-    }
-});
 
-
-app.get('/getComments/:id', function(req, res) {
-    CommentModel.find({"imageId": req.params.id}, function(err, comments) {
-        if (err) {
-            console.log(err);
-            res.send(err);
-        } else if (!comments) {
-            console.log("Image id not found");
-            res.send("Image id not found");
-        } else {
-            const response = {
-                statusCode: 200,
-                comments: comments
-            }
-            console.log(comments);
-            res.send(comments);
-        }
-    });
-});
-
-
-app.delete('/deleteComment/:id', function(req, res) {
-    CommentModel.findByIdAndRemove(req.params.id, function(err, removed) {
-        if (err) {
-            console.log(err);
-            res.send(err);
-        } else if (!removed) {
-            console.log("Comment not found to delete");
-            res.send("Comment not found to delete");
-        } else {
-            console.log("Comment removed: ", removed);
-            res.send({ statusCode: 200 });
-        }
-    });
-});
-
-
-app.post('/createUser', function(req, res){
-if (req.body.email && req.body.username &&
-    req.body.password && req.body.passwordConf) {
-        var userData = {
-            email: req.body.email,
-            username: req.body.username,
-            password: req.body.password,
-            passwordConf: req.body.passwordConf,
-        }
-        User.create(userData, function (err, user) {
-            if (err) {
-                console.log(err);
-                res.send(err);
-            } else {
-                console.log("Created user " + req.body.username);
-                res.send({ statusCode: 200 });
-            }
-        });
-    }
-});
-
-
-app.get('/listUsers', function(req, res) {
-    User.find(function(err, users) {
-        if (err) {
-            console.log(err);
-            res.send(err);
-        } else {
-            console.log(users);
-            res.send(users);
-        }
-    });
-});
-
-
-app.get('/signIn/:username/:password', function(req, res) {
-    User.findOne({"username": req.params.username}, function(err, user) {
-        if (err) {
-            console.log(err);
-            res.send(err);
-        } else if (!user) {
-            console.log("Didn't find user");
-            res.send("Didn't find user");
-        } else {
-            if (req.params.password === user.password) {
-                console.log(user.username, " signed in");
-                res.send({statusCode: 200});
-            } else {
-                console.log(user, user.username, user.password, req.params.username, req.params.password);
-                console.log("Password doesn't match");
-                res.send("Password doesn't match");
-            }
-        }
-    });
-});
-
-
-app.get('/getImage/:id', (req, res) => {
-    ImageModel.findOne({"imageId": req.params.id}, function(err, dbEntry) {
-        if (err) {
-            console.log(err);
-            res.send(err);
-        } else {
-            var params = {
-                Bucket: 'memesourceimages',
-                Key : req.params.id+'.jpg'
-            };
-            // s3.getObject(params, function (err, data) {
-            //     if (err) {
-            //         console.log("Error: ", err);
-            //         res.send(err);
-            //     }
-            //     if (data) {
-            //         data.Metadata = dbEntry;
-            //         const response = {
-            //             statusCode: 200,
-            //             data: data
-            //         }
-            //         console.log("Data: ", data);
-            //         res.send(response);
-            //     }
-            // });
-            console.log(dbEntry);
-            res.send(dbEntry);
-        }
-    });
-});
 
 app.get('/', (req, res) => {
     console.log('Welcome to memesource');
