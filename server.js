3af0ac6b966a1891343d121c4973f9fa0ef44fb4
Doren Proctor <dorenproctor@gmail.com>
2018-06-27 00:46:51 -0700

    SignIn is done with a POST request using bcrypt
    Yay for secure user authentication!

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const s3 = new AWS.S3();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const schemas = require('./schemas');
+const bcrypt = require('bcrypt');
 
 var User = mongoose.model('User', schemas.User);
 var Comment = mongoose.model('Comment', schemas.Comment);
@@ -21,7 +22,7 @@ mongoose.connect('mongodb://localhost/ms');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'Failed to connect to database'));
 db.once('open', function () {
-    console.log("Connected to Database");
+  console.log("Connected to Database");
 });
 
 /* available commands:
@@ -39,174 +40,185 @@ db.once('open', function () {
 */
 
 
-app.get('/imageInfo/:id', function(req, res) {
-    Image.findOne({"imageId": req.params.id}, function(err, dbEntry) {
-        if (err) {
-            console.log(err);
-            res.send(err);
-        } else {
-            console.log(dbEntry);
-            res.send(dbEntry);
-        }
-    });
+app.get('/imageInfo/:id', function (req, res) {
+  Image.findOne({ "imageId": req.params.id }, function (err, dbEntry) {
+    if (err) {
+      console.log(err);
+      res.send(err);
+    } else {
+      console.log(dbEntry);
+      res.send(dbEntry);
+    }
+  });
 });
 
 
-app.get('/getImage/:id', function(req, res) {
-    // var params = {
-    //     Bucket: 'memesourceimages',
-    //     Key : req.params.id+'.jpg'
-    // };
-    // s3.getObject(params, function (err, data) {
-    //     if (err) {
-    //         console.log("Error: ", err);
-    //         res.send(err);
-    //     }
-    //     if (data) {
-    //         // data.Metadata = dbEntry;
-    //         // const response = {
-    //         //         statusCode: 200,
-    //         //     data: data
-    //         // }
-    //         console.log("Data: ", data);
-    //         res.send(data);
-    //     }
-    // });
-    console.log("Sending image " + req.params.id);
-    var img = fs.readFileSync('./img/' + req.params.id + '.jpg');
-    res.writeHead(200, { 'Content-Type': 'image/jpg' });
-    res.end(img, 'binary');
+app.get('/getImage/:id', function (req, res) {
+  // var params = {
+  //     Bucket: 'memesourceimages',
+  //     Key : req.params.id+'.jpg'
+  // };
+  // s3.getObject(params, function (err, data) {
+  //     if (err) {
+  //         console.log("Error: ", err);
+  //         res.send(err);
+  //     }
+  //     if (data) {
+  //         // data.Metadata = dbEntry;
+  //         // const response = {
+  //         //         statusCode: 200,
+  //         //     data: data
+  //         // }
+  //         console.log("Data: ", data);
+  //         res.send(data);
+  //     }
+  // });
+  console.log("Sending image " + req.params.id);
+  var img = fs.readFileSync('./img/' + req.params.id + '.jpg');
+  res.writeHead(200, { 'Content-Type': 'image/jpg' });
+  res.end(img, 'binary');
 });
 
 app.get('/urls', function (req, res) {
-    Image.find({}, { imageId: 1, _id: 0 }, function (err, imgs) {
-        if (err) {
-            console.log(err);
-            res.send(err);
-        } else {
-            var urls = imgs.map(obj => {
-                var newObj = {};
-                var string = "http://ec2-18-188-44-41.us-east-2.compute.amazonaws.com/getImage/" + obj.imageId;
-                newObj["url"] = string;
-                return newObj;
-            });
-            res.send({ urls: urls });
-        }
-    }).sort({ imageId: 1 });
+  Image.find({}, { imageId: 1, _id: 0 }, function (err, imgs) {
+    if (err) {
+      console.log(err);
+      res.send(err);
+    } else {
+      var urls = imgs.map(obj => {
+        var newObj = {};
+        var string = "http://ec2-18-188-44-41.us-east-2.compute.amazonaws.com/getImage/" + obj.imageId;
+        newObj["url"] = string;
+        return newObj;
+      });
+      res.send({ urls: urls });
+    }
+  }).sort({ imageId: 1 });
 });
 
 
 app.get('/commentInfo/:id', function (req, res) {
-    Comment.find({ "imageId": req.params.id }, function (err, comments) {
-        if (err) {
-            console.log(err);
-            res.send(err);
-        } else if (!comments) {
-            console.log("Image id not found");
-            res.send("Image id not found");
-        } else {
-            // const response = {
-            //     statusCode: 200,
-            //     comments: comments
-            // }
-            console.log(comments);
-            res.send(comments);
-        }
-    });
+  Comment.find({ "imageId": req.params.id }, function (err, comments) {
+    if (err) {
+      console.log(err);
+      res.send(err);
+    } else if (!comments) {
+      console.log("Image id not found");
+      res.send("Image id not found");
+    } else {
+      // const response = {
+      //     statusCode: 200,
+      //     comments: comments
+      // }
+      console.log(comments);
+      res.send(comments);
+    }
+  });
 });
 
 
 app.post('/postComment', function (req, res) {
-    if (req.body.imageId && req.body.content && req.body.user) {
-        var commentData = {
-            imageId: req.body.imageId,
-            content: req.body.content,
-            user: req.body.user
-        };
-        Comment.create(commentData, function (err, user) {
-            if (err) {
-                console.log(err);
-                res.send(err);
-            } else {
-                console.log("Created comment", req.body.content);
-                res.send({ statusCode: 200 });
-            }
-        });
-    } else {
-        res.send("The proper parameters were not passed");
-    }
+  if (req.body.imageId && req.body.content && req.body.user) {
+    var commentData = {
+      imageId: req.body.imageId,
+      content: req.body.content,
+      user: req.body.user
+    };
+    Comment.create(commentData, function (err, user) {
+      if (err) {
+        console.log(err);
+        res.send(err);
+      } else {
+        console.log("Created comment", req.body.content);
+        res.send({ statusCode: 200 });
+      }
+    });
+  } else {
+    res.send("The proper parameters were not passed");
+  }
 });
 
 
 app.delete('/deleteComment/:id', function (req, res) {
-    Comment.findByIdAndRemove(req.params.id, function (err, removed) {
-        if (err) {
-            console.log(err);
-            res.send(err);
-        } else if (!removed) {
-            console.log("Comment not found to delete");
-            res.send("Comment not found to delete");
-        } else {
-            console.log("Comment removed: ", removed);
-            res.send({ statusCode: 200 });
-        }
-    });
+  Comment.findByIdAndRemove(req.params.id, function (err, removed) {
+    if (err) {
+      console.log(err);
+      res.send(err);
+    } else if (!removed) {
+      console.log("Comment not found to delete");
+      res.send("Comment not found to delete");
+    } else {
+      console.log("Comment removed: ", removed);
+      res.send({ statusCode: 200 });
+    }
+  });
 });
 
 
 app.post('/createUser', function (req, res) {
-    if (req.body.email && req.body.username &&
-        req.body.password) {
+  if (!req.body.email || !req.body.username ||
+    !req.body.password) {
+      res.send("Email, username and password required");
+  } else {
+    bcrypt.hash(req.body.password, 12).then(function(hash) {
+      console.log("ASFSDFSDF ", hash);
       var userData = {
-          email: req.body.email,
-          username: req.body.username,
-          password: req.body.password,
+        email: req.body.email,
+        username: req.body.username,
+        password: hash,
       }
       User.create(userData, function (err, user) {
-          if (err) {
-              console.log(err);
-              res.send(err);
-          } else {
-              console.log("Created user " + req.body.username);
-              res.send({ statusCode: 200 });
-          }
+        if (err) {
+          console.log(err);
+          res.send(err);
+        } else {
+          console.log("Created user " + req.body.username);
+          res.send({ statusCode: 200 });
+        }
       });
-    }
+    });
+  }
 });
 
 
 app.get('/listUsers', function (req, res) {
-    User.find(function (err, users) {
-        if (err) {
-            console.log(err);
-            res.send(err);
-        } else {
-            console.log(users);
-            res.send(users);
-        }
-    });
+  User.find(function (err, users) {
+    if (err) {
+      console.log(err);
+      res.send(err);
+    } else {
+      console.log(users);
+      res.send(users);
+    }
+  });
 });
 
 
-app.get('/signIn/:username/:password', function (req, res) {
-    User.findOne({ "username": req.params.username }, function (err, user) {
-        if (err) {
-            console.log(err);
-            res.send(err);
-        } else if (!user) {
-            console.log("Didn't find user");
-            res.send("Didn't find user");
+app.post('/signIn', function (req, res) {
+  if (!req.body.username || !req.body.password) {
+    res.send("Username and password required");
+  }
+  const username = req.body.username;
+  const password = req.body.password;
+  User.findOne({ "username": username }, function (err, user) {
+    if (err) {
+      console.log(err);
+      res.send(err);
+    } else if (!user) {
+      console.log("Didn't find user: ", username);
+      res.send("Didn't find user");
+    } else {
+      bcrypt.compare(password, user.password).then(function(result) {
+        if (result) {
+          console.log(user.username, " signed in");
+          res.send({ statusCode: 200 });
         } else {
-            if (req.params.password === user.password) {
-                console.log(user.username, " signed in");
-                res.send({ statusCode: 200 });
-            } else {
-                console.log(user, user.username, user.password, req.params.username, req.params.password);
-                console.log("Password doesn't match");
-                res.send("Password doesn't match");
-            }
+          console.log("Password doesn't match");
+          res.send("Password doesn't match");
         }
-    });
+      });
+    }
+  });
 });
 
 
@@ -214,116 +226,116 @@ app.get('/signIn/:username/:password', function (req, res) {
 
 
 app.put('/upvoteImage/:username/:imageId', function (req, res) {
-    var username = req.params.username;
-    var imageId = req.params.imageId;
-    var upvoted = false;
-    var downvoted = false;
-    Image.findOne({ "imageId": imageId }, function (err, img) {
-        if (err) {
+  var username = req.params.username;
+  var imageId = req.params.imageId;
+  var upvoted = false;
+  var downvoted = false;
+  Image.findOne({ "imageId": imageId }, function (err, img) {
+    if (err) {
+      console.log(err);
+      res.send(err);
+    } else if (!img) {
+      console.log("Couldn't find image");
+      res.send("Couldn't find image");
+    } else {
+      if (img.upvoters.includes(username)) { upvoted = true; }
+      if (img.downvoters.includes(username)) { downvoted = true; }
+      if (!upvoted) { //add upvote
+        Image.updateOne({ "imageId": imageId }, { $push: { upvoters: username }, $inc: { upvotes: 1 } }, function (err, data) {
+          if (err) {
             console.log(err);
             res.send(err);
-        } else if (!img) {
-            console.log("Couldn't find image");
-            res.send("Couldn't find image");
-        } else {
-            if (img.upvoters.includes(username)) { upvoted = true; }
-            if (img.downvoters.includes(username)) { downvoted = true; }
-            if (!upvoted) { //add upvote
-                Image.updateOne({ "imageId": imageId }, { $push: { upvoters: username }, $inc: { upvotes: 1 } }, function (err, data) {
-                    if (err) {
-                        console.log(err);
-                        res.send(err);
-                        return;
-                    } else {
-                        console.log(username + " upvoted " + imageId);
-                    }
-                });
-            } else { //already upvoted. remove it
-                Image.updateOne({ "imageId": imageId }, { $pull: { upvoters: username }, $inc: { upvotes: -1 } }, function (err, data) {
-                    if (err) {
-                        console.log(err);
-                        res.send(err);
-                        return;
-                    } else {
-                        console.log(username + " removed upvote on Image" + imageId);
-                    }
-                });
-            }
-            if (downvoted) { //remove downvote
-                Image.updateOne({ "imageId": imageId }, { $pull: { downvoters: username }, $inc: { downvotes: -1 } }, function (err, data) {
-                    if (err) {
-                        console.log(err);
-                        res.send(err);
-                        return;
-                    } else {
-                        console.log("Downvote on Image " + imageId + " by " + username + " removed because of upvote");
-                    }
-                });
-            }
-            res.send({ statusCode: 200 });
-        }
-    });
+            return;
+          } else {
+            console.log(username + " upvoted " + imageId);
+          }
+        });
+      } else { //already upvoted. remove it
+        Image.updateOne({ "imageId": imageId }, { $pull: { upvoters: username }, $inc: { upvotes: -1 } }, function (err, data) {
+          if (err) {
+            console.log(err);
+            res.send(err);
+            return;
+          } else {
+            console.log(username + " removed upvote on Image" + imageId);
+          }
+        });
+      }
+      if (downvoted) { //remove downvote
+        Image.updateOne({ "imageId": imageId }, { $pull: { downvoters: username }, $inc: { downvotes: -1 } }, function (err, data) {
+          if (err) {
+            console.log(err);
+            res.send(err);
+            return;
+          } else {
+            console.log("Downvote on Image " + imageId + " by " + username + " removed because of upvote");
+          }
+        });
+      }
+      res.send({ statusCode: 200 });
+    }
+  });
 });
 
 
 app.put('/downvoteImage/:username/:imageId', function (req, res) {
-    var username = req.params.username;
-    var imageId = req.params.imageId;
-    var upvoted = false;
-    var downvoted = false;
-    Image.findOne({ "imageId": imageId }, function (err, img) {
-        if (err) {
+  var username = req.params.username;
+  var imageId = req.params.imageId;
+  var upvoted = false;
+  var downvoted = false;
+  Image.findOne({ "imageId": imageId }, function (err, img) {
+    if (err) {
+      console.log(err);
+      res.send(err);
+    } else if (!img) {
+      console.log("Couldn't find image");
+      res.send("Couldn't find image");
+    } else {
+      if (img.upvoters.includes(username)) { upvoted = true; }
+      if (img.downvoters.includes(username)) { downvoted = true; }
+      if (!downvoted) { //add upvote
+        Image.updateOne({ "imageId": imageId }, { $push: { downvoters: username }, $inc: { downvotes: 1 } }, function (err, data) {
+          if (err) {
             console.log(err);
             res.send(err);
-        } else if (!img) {
-            console.log("Couldn't find image");
-            res.send("Couldn't find image");
-        } else {
-            if (img.upvoters.includes(username)) { upvoted = true; }
-            if (img.downvoters.includes(username)) { downvoted = true; }
-            if (!downvoted) { //add upvote
-                Image.updateOne({ "imageId": imageId }, { $push: { downvoters: username }, $inc: { downvotes: 1 } }, function (err, data) {
-                    if (err) {
-                        console.log(err);
-                        res.send(err);
-                        return;
-                    } else {
-                        console.log(username + " downvoted " + imageId);
-                    }
-                });
-            } else { //already upvoted. remove it
-                Image.updateOne({ "imageId": imageId }, { $pull: { downvoters: username }, $inc: { downvotes: -1 } }, function (err, data) {
-                    if (err) {
-                        console.log(err);
-                        res.send(err);
-                        return;
-                    } else {
-                        console.log(username + " removed downvote on Image" + imageId);
-                    }
-                });
-            }
-            if (upvoted) { //remove downvote
-                Image.updateOne({ "imageId": imageId }, { $pull: { upvoters: username }, $inc: { upvotes: -1 } }, function (err, data) {
-                    if (err) {
-                        console.log(err);
-                        res.send(err);
-                        return;
-                    } else {
-                        console.log("Upvote on Image " + imageId + " by " + username + " removed because of downvote");
-                    }
-                });
-            }
-            res.send({ statusCode: 200 });
-        }
-    });
+            return;
+          } else {
+            console.log(username + " downvoted " + imageId);
+          }
+        });
+      } else { //already upvoted. remove it
+        Image.updateOne({ "imageId": imageId }, { $pull: { downvoters: username }, $inc: { downvotes: -1 } }, function (err, data) {
+          if (err) {
+            console.log(err);
+            res.send(err);
+            return;
+          } else {
+            console.log(username + " removed downvote on Image" + imageId);
+          }
+        });
+      }
+      if (upvoted) { //remove downvote
+        Image.updateOne({ "imageId": imageId }, { $pull: { upvoters: username }, $inc: { upvotes: -1 } }, function (err, data) {
+          if (err) {
+            console.log(err);
+            res.send(err);
+            return;
+          } else {
+            console.log("Upvote on Image " + imageId + " by " + username + " removed because of downvote");
+          }
+        });
+      }
+      res.send({ statusCode: 200 });
+    }
+  });
 });
 
 
 
 
 app.get('/', (req, res) => {
-    console.log('Welcome to memesource');
-    res.send('Welcome to memesource');
+  console.log('Welcome to memesource');
+  res.send('Welcome to memesource');
 })
 
 app.listen(3000, () => console.log('Server is running'));
